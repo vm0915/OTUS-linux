1e46927c1606d5ea3632b7239240ca0d23b09eec
Vladimir <v.s.martyanov@yandex.ru>
2020-05-04 19:04:58 +0300

    Initial commit

---
+++ HW1/asteriks/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir /home/vagrant/coretest
+cd /home/vagrant/coretest
+
+sudo yum update -y && \
+sudo yum install -y wget flex bison bc openssl-devel gcc libelf-devel elfutils-libelf-devel perl 
+
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.6.10.tar.xz
+DISTRO='linux-5.6.10'
+export $DISTRO
+tar xf $DISTRO.tar.xz
+cd /home/vagrant/coretest/$DISTRO
+
+sudo cp /boot/config-$(uname -r) .config
+sudo make olddefconfig && \
+sudo make -j16 && \
+sudo make -j16 O=$BUILD modules && \
+sudo make O=$BUILD modules_install && sudo make O=$BUILD install
+
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-set-default 0
+sudo ls /boot
+echo ***END OF PROVISIONING***
