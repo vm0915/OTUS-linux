a37252dc01b9bc849d11fa37599ccc45101cd02b
user01 <user01@srv-centos-test.localdomain>
2020-09-10 17:47:44 +0300

    Added .sh comments

--- HW10_proc/mypsax.sh
+++ HW10_proc/mypsax.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# Для чтения /proc необходимо иметь права root
 
+# Перенаправим вывод ошибок 
 exec 2>/dev/null
 
+# Напечатаем заголовок таблицы
 echo "PID;TTY;STAT;TIME;COMMAND" | column -t -s ";"
 
+## Вычисление процессорного времени выделим в отдельную функцию для удобства
 timecalc(){
 # uptime of the system (seconds)
 uptime=$(cat /proc/uptime | awk '{print $1}')
@@ -14,46 +18,46 @@ utime=$(cat /proc/$1/stat | awk '{print $14}')
 # CPU time spent in kernel code, measured in clock ticks
 stime=$(cat /proc/$1/stat | awk '{print $15}')
 
-# Waited-for children's CPU time spent in user code (in clock ticks)
-cutime=$(cat /proc/$1/stat | awk '{print $16}')
-
-# Waited-for children's CPU time spent in kernel code (in clock ticks)
-cstime=$(cat /proc/$1/stat | awk '{print $17}')
-
 # Time when the process started, measured in clock ticks
 starttime=$(cat /proc/$1/stat | awk '{print $22}')
 
 # Hertz (number of clock ticks per second) of the system
 hertz=$(getconf CLK_TCK)
 
-#total_time=$(echo $utime $stime | awk '{print $1 + $2}')
-#total_time=$(echo $total_time $cutime $cstime | awk '{print $1 + $2 + $3}')
-#seconds=$((uptime - (starttime / Hertz)))
-#seconds=$(echo $uptime $starttime $hertz | awk '{print ($1 - $2 / $3)}')
-# cpu_usage = 100 * ((total_time / Hertz) / seconds)
+# Полное время выполнения процесса в тактах
 total_time=$((utime + stime))
+
+# Полное время выполнения процесса в секундах
 seconds_total=$((total_time / hertz))
+
+# Задаем формат М:S
 minutes=$((seconds_total / 60))
 seconds=$((seconds_total % 60))
 TIME=$(echo "$minutes:$seconds")
-
 }
 
+# Для всех директорий с числовым названием в /proc 
 for PID in $(ls /proc | grep ^[0-9] | sort -g)
 do
+# Связанный с процессом терминал определим с помощью нулевого дескриптора 
 TTY=$(ls -l /proc/$PID/fd | tail --lines=+2 | head -1 | awk '{print $11}'  | cut -d "/" -f 3,4)
+	# Сравниваем переменную с regex выражением, чтобы понять, связан ли процесс с терминалом
 	if [[ ! ${TTY} =~ (pts\/).* ]]
 	then
 	TTY="?"
 	fi
+# Смотрим состояние процесса
 STAT=$(cat /proc/$PID/stat | awk '{print $3}')
+# Производим расчеты времени
 timecalc $PID
+# Смотрим команду запустившую процесс
 COMMAND=$(cat /proc/$PID/cmdline)
+# Если ее нет в cmdline, то берем из строки "Name:" в файле status
 if [ -z "$COMMAND" ]
 then
 COMMAND=$(cat /proc/$PID/status | grep "Name:" | awk '{print $2}')
 COMMAND="["+$COMMAND+"]"
 fi
+# Выводим результат итерации, разделяя на столбцы
 echo "$PID;$TTY;$STAT;$TIME;$COMMAND" | column -t -s ";" 
 done
-echo $TEXT | column -t -s ";"
