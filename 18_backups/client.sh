aa2178a5d7372fe7264c205cefabc62e76261c21
root <root@localhost.localdomain>
2020-11-18 15:22:28 -0500

    update

--- 18_backups/client.sh
+++ 18_backups/client.sh
@@ -7,15 +7,15 @@ yum install -y borgbackup
 
 # copy keys 
 
-chmod 600 /vagrant/private_key
-chown root /vagrant/script.sh
-chmod 700 /vagrant/script.sh
+chmod 600 /home/vagrant/private_key
+chown root /home/vagrant/script.sh
+chmod 700 /home/vagrant/script.sh
 
 
 sudo BORG_PASSPHRASE="otus" \
-BORG_RSH="ssh -o 'StrictHostKeyChecking=no' -i /vagrant/private_key" \
+BORG_RSH="ssh -o 'StrictHostKeyChecking=no' -i /home/vagrant/private_key" \
 borg create -v --stats root@192.168.11.10:/var/backup::'{now:%Y-%m-%d-%H-%M}' /etc \
 2>&1 | logger &
 
 # create cron
-sudo echo "*/5 * * * * root /vagrant/script.sh" > /etc/cron.d/borg-backup 
+sudo echo "*/5 * * * * root /home/vagrant/script.sh" > /etc/cron.d/borg-backup 
--- 18_backups/script.sh
+++ 18_backups/script.sh
@@ -6,12 +6,18 @@ SERVER=192.168.11.10
 
 # Backup type, it may be data, system, mysql, binlogs, etc.
 TYPEOFBACKUP=etc
-REPOSITORY=$CLIENT@$SERVER:/var/backup/$(hostname)-${TYPEOFBACKUP}
+REPOSITORY=$CLIENT@$SERVER:/var/backup/
 
 # Backup
 borg create -v --stats \
  $REPOSITORY::'{now:%Y-%m-%d-%H-%M}' \
  /etc
+
+BORG_PASSPHRASE="otus" \
+BORG_RSH="ssh -o 'StrictHostKeyChecking=no' -i /vagrant/private_key" \
+borg create -v --stats $REPOSITORY::'{now:%Y-%m-%d-%H-%M}' /etc \
+2>&1 | logger &
+
 
 # After backup
 borg prune -v --show-rc --list $REPOSITORY \
